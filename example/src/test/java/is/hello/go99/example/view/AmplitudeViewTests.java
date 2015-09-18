c3118f4320d016856c317e93a075bb691ff5efad
km <km@sayhello.com>
2015-09-18 16:16:36 -0700

    this will probably pass

--- example/src/test/java/is/hello/go99/example/view/AmplitudeViewTests.java
+++ example/src/test/java/is/hello/go99/example/view/AmplitudeViewTests.java
@@ -18,37 +18,37 @@ public class AmplitudeViewTests extends ExampleTestCase {
 
     @Test
     public void getColor() {
-        assertThat(colors.getColor(0.0f, 0xff), is(equalTo(0xffE3F2FD)));
-        assertThat(colors.getColor(0.0f, 0x88), is(equalTo(0x88E3F2FD)));
-        assertThat(colors.getColor(0.5f, 0xff), is(equalTo(0xffBBDEFB)));
-        assertThat(colors.getColor(0.5f, 0x88), is(equalTo(0x88BBDEFB)));
-        assertThat(colors.getColor(0.8f, 0xff), is(equalTo(0xff90CAF9)));
-        assertThat(colors.getColor(0.8f, 0x88), is(equalTo(0x8890CAF9)));
+        assertThat(colors.getColor(0.0f, 0xff), is(equalTo(0xffBBDEFB)));
+        assertThat(colors.getColor(0.0f, 0x88), is(equalTo(0x88BBDEFB)));
+        assertThat(colors.getColor(0.5f, 0xff), is(equalTo(0xff90CAF9)));
+        assertThat(colors.getColor(0.5f, 0x88), is(equalTo(0x8890CAF9)));
+        assertThat(colors.getColor(0.8f, 0xff), is(equalTo(0xff64B5F6)));
+        assertThat(colors.getColor(0.8f, 0x88), is(equalTo(0x8864B5F6)));
     }
 
     @Test
     public void getAnimatorColorsIncremental() {
         assertThat(colors.getAnimatorColors(0.0f, 1.0f),
-                   is(equalTo(new int[] { 0xffE3F2FD, 0xffBBDEFB, 0xff90CAF9 } )));
+                   is(equalTo(new int[] { 0xffBBDEFB, 0xff90CAF9, 0xff64B5F6} )));
         assertThat(colors.getAnimatorColors(0.0f, 0.5f),
-                   is(equalTo(new int[] { 0xffE3F2FD, 0xffBBDEFB })));
+                   is(equalTo(new int[] { 0xffBBDEFB, 0xff90CAF9 })));
     }
 
     @Test
     public void getAnimatorColorsDecremental() {
         assertThat(colors.getAnimatorColors(1.0f, 0.0f),
-                   is(equalTo(new int[] { 0xff90CAF9, 0xffBBDEFB, 0xffE3F2FD } )));
+                   is(equalTo(new int[] { 0xff64B5F6, 0xff90CAF9, 0xffBBDEFB } )));
         assertThat(colors.getAnimatorColors(0.5f, 0.0f),
-                   is(equalTo(new int[] { 0xffBBDEFB, 0xffE3F2FD, })));
+                   is(equalTo(new int[] { 0xff90CAF9, 0xffBBDEFB, })));
     }
 
     @Test
     public void getAnimatorColorsSame() {
         assertThat(colors.getAnimatorColors(1.0f, 1.0f),
-                   is(equalTo(new int[] { 0xff90CAF9, 0xff90CAF9 } )));
+                   is(equalTo(new int[] { 0xff64B5F6, 0xff64B5F6} )));
         assertThat(colors.getAnimatorColors(0.5f, 0.5f),
-                   is(equalTo(new int[] { 0xffBBDEFB, 0xffBBDEFB })));
+                   is(equalTo(new int[] { 0xff90CAF9, 0xff90CAF9 })));
         assertThat(colors.getAnimatorColors(0.0f, 0.0f),
-                   is(equalTo(new int[] { 0xffE3F2FD, 0xffE3F2FD })));
+                   is(equalTo(new int[] { 0xffBBDEFB, 0xffBBDEFB })));
     }
 }
