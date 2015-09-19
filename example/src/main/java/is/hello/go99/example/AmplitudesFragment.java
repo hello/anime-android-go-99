26e36eb2f3cb358b54b11c70f164e85a594d0617
km <km@sayhello.com>
2015-09-18 17:13:36 -0700

    work in progress example tooltips

--- example/src/main/java/is/hello/go99/example/AmplitudesFragment.java
+++ example/src/main/java/is/hello/go99/example/AmplitudesFragment.java
@@ -2,7 +2,10 @@ package is.hello.go99.example;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,15 +15,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import is.hello.go99.Anime;
 import is.hello.go99.animators.AnimatorContext;
+import is.hello.go99.animators.AnimatorTemplate;
+import is.hello.go99.animators.OnAnimationCompleted;
 import is.hello.go99.example.data.AmplitudeSource;
 import is.hello.go99.example.data.RandomAmplitudeSource;
 import is.hello.go99.example.recycler.AmplitudeAdapter;
 import is.hello.go99.example.recycler.AmplitudeItemAnimator;
+import is.hello.go99.example.view.InfoTooltipPopup;
 
 public class AmplitudesFragment extends Fragment
         implements AnimatorContext.Scene, SwipeRefreshLayout.OnRefreshListener,
-        AmplitudeSource.Consumer {
+        AmplitudeSource.Consumer, AmplitudeAdapter.OnClickListener {
+    private static final long DELAY_STEP = 10;
+    private static final float TARGET_DIMMED_ALPHA = 0.25f;
+
     private static final String SAVED_SOURCE_STATE = AmplitudesFragment.class.getName() + "#SAVED_SOURCE_STATE";
 
     private final AnimatorContext animatorContext = new AnimatorContext(getClass().getSimpleName());
@@ -30,6 +40,8 @@ public class AmplitudesFragment extends Fragment
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
     private AmplitudeAdapter adapter;
+
+    private @Nullable InfoTooltipPopup infoTooltipPopup;
 
 
     //region Lifecycle
@@ -64,7 +76,7 @@ public class AmplitudesFragment extends Fragment
 
         recyclerView.setItemAnimator(new AmplitudeItemAnimator(getAnimatorContext()));
 
-        this.adapter = new AmplitudeAdapter(getResources());
+        this.adapter = new AmplitudeAdapter(getResources(), this);
         recyclerView.setAdapter(adapter);
 
         return view;
@@ -138,6 +150,86 @@ public class AmplitudesFragment extends Fragment
         builder.setPositiveButton(android.R.string.ok, null);
         builder.setCancelable(true);
         builder.create().show();
+    }
+
+    //endregion
+
+
+    //region Item Animations
+
+    @Override
+    public void onItemClicked(@NonNull AmplitudeAdapter.ViewHolder viewHolder) {
+        cascadeFadeOutFromCenterItem(viewHolder);
+    }
+
+    private void cascadeFadeOutFromCenterItem(@NonNull final AmplitudeAdapter.ViewHolder viewHolder) {
+        final int childIndex = recyclerView.indexOfChild(viewHolder.itemView);
+        if (childIndex == -1) {
+            Log.w(getClass().getSimpleName(), "Child view is missing?!");
+            return;
+        }
+
+        final AnimatorTemplate animatorTemplate = new AnimatorTemplate(Anime.DURATION_FAST,
+                                                                       new FastOutLinearInInterpolator());
+        getAnimatorContext().transaction(animatorTemplate, AnimatorContext.OPTIONS_DEFAULT, new AnimatorContext.TransactionConsumer() {
+            @Override
+            public void consume(@NonNull AnimatorContext.Transaction transaction) {
+                long delay = 0;
+                for (int i = childIndex - 1; i >= 0; i--) {
+                    transaction.animatorFor(recyclerView.getChildAt(i))
+                               .withStartDelay(delay)
+                               .alpha(TARGET_DIMMED_ALPHA);
+
+                    delay += DELAY_STEP;
+                }
+
+                transaction.animatorFor(viewHolder.itemView)
+                           .alpha(1f);
+
+                delay = 0;
+                for (int i = childIndex + 1, count = recyclerView.getChildCount(); i < count; i++) {
+                    transaction.animatorFor(recyclerView.getChildAt(i))
+                               .withStartDelay(delay)
+                               .alpha(TARGET_DIMMED_ALPHA);
+                    delay += DELAY_STEP;
+                }
+
+                if (infoTooltipPopup != null) {
+                    infoTooltipPopup.dismiss();
+                }
+
+                AmplitudesFragment.this.infoTooltipPopup = new InfoTooltipPopup(getActivity(), new Runnable() {
+                    @Override
+                    public void run() {
+                        cascadeFadeIn();
+                    }
+                });
+                infoTooltipPopup.setText("Hello, world");
+                infoTooltipPopup.show(viewHolder.itemView);
+            }
+        }, new OnAnimationCompleted() {
+            @Override
+            public void onAnimationCompleted(boolean finished) {
+            }
+        });
+    }
+
+    private void cascadeFadeIn() {
+        final AnimatorTemplate animatorTemplate = new AnimatorTemplate(Anime.DURATION_FAST,
+                                                                       new FastOutSlowInInterpolator());
+        getAnimatorContext().transaction(animatorTemplate, AnimatorContext.OPTIONS_DEFAULT, new AnimatorContext.TransactionConsumer() {
+            @Override
+            public void consume(@NonNull AnimatorContext.Transaction transaction) {
+                for (int i = 0, count = recyclerView.getChildCount(); i < count; i++) {
+                    transaction.animatorFor(recyclerView.getChildAt(i))
+                               .alpha(1f);
+                }
+            }
+        }, new OnAnimationCompleted() {
+            @Override
+            public void onAnimationCompleted(boolean finished) {
+            }
+        });
     }
 
     //endregion
